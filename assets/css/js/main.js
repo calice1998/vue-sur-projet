3359185088b0c71f95484037c1ecf7327bd1576e
hodjiadoum calice <hodjiadoumcalice@gmail.com>
2023-04-25 10:58:39 +0400

    first commit

---
+++ assets/css/js/main.js
@@ -0,0 +1,9 @@
+console.log("MAIN");
+
+const iconToggleFullNames = document.getElementById("toggleFullNames")
+
+iconToggleFullNames.addEventListener("click",()=>{
+    //console.log("click");
+    const fullNames = document.getElementsByClassName("infosleft")[infosleft]
+    fullNames.classList.toggle("none")
+})
